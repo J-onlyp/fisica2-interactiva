5d3087134df02fa5343650ca7aeb33589e70b49f
J-onlyp <sandroc1189@gmail.com>
2025-11-27 19:45:36 -0500

    Deploy GitHub Pages solo Electricidad

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/fisica2-interactiva/",
+  base: "/fisica2-interactiva/", 
   build: {
-    outDir: "docs",
+    outDir: "docs", 
   },
 });
